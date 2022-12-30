a4c2a699729f21953cf5a0fc468dfc387d25e60b
Nir Tamir <nirtamir2@gmail.com>
2022-12-30 23:11:07 +0200

    add @routes-gen/solid-start

---
+++ packages/routes-gen-solid-start/README.md
@@ -0,0 +1,17 @@
+# @routes-gen/solid-start
+
+This package is a [**routes-gen**](https://github.com/sandulat/routes-gen) driver for [SolidStart](https://github.com/solidjs/solid-start).
+
+## Installation
+
+```
+yarn add routes-gen @routes-gen/solid-start
+```
+
+## Usage
+
+```
+yarn routes-gen -d @routes-gen/solud-start
+```
+
+By default, it exports the routes to: `${appDirectory}/routes.d.ts`.
---
+++ packages/routes-gen-solid-start/src/index.ts
@@ -0,0 +1,40 @@
+import path from "path";
+import {exec} from "child_process"
+import {Driver} from "routes-gen/src";
+
+const appDirectory = "./src";
+export const defaultOutputPath: Driver["defaultOutputPath"] = `${appDirectory}/routes.d.ts`;
+export const watchPaths: Driver["watchPaths"] = async () => [
+  `${appDirectory}/routes/**/*.{ts,tsx,js,jsx}`,
+];
+
+type SolidStartRouteData = {
+  id: string,
+  path: string,
+  componentPath: string,
+  dataPath: string
+};
+export const routes: Driver["routes"] = async () => {
+  const scriptPath = path.join(__dirname, 'node_modules', 'solid-start', 'bin.cjs');
+  const parsedRoutes = await new Promise<Array<SolidStartRouteData>>(async (resolve) => {
+    exec(`node ${scriptPath} routes`, (error, stdout, stderr) => {
+      if (error != null) {
+        console.error("error", error);
+        return;
+      }
+      const routes = JSON.parse(stdout) as Array<SolidStartRouteData>;
+      resolve(routes);
+    });
+  });
+  if (parsedRoutes.length === 0) {
+    throw new Error('Couldn\'t parse routes. This may be due to breaking changes in "solid-start".');
+  }
+  return parsedRoutes;
+};
+
+
+module.exports = {
+  defaultOutputPath,
+  watchPaths,
+  routes
+}
