f06ae2101124f218fa6dc8ec744509704cdddf68
Nir Tamir <nirtamir2@gmail.com>
2023-01-03 23:51:30 +0200

    filter * routes

--- packages/routes-gen-solid-start/src/index.ts
+++ packages/routes-gen-solid-start/src/index.ts
@@ -1,5 +1,5 @@
-import {exec} from "child_process"
-import {Driver} from "routes-gen/src";
+import { exec } from "child_process";
+import { Driver } from "routes-gen/src";
 
 const appDirectory = "./src";
 export const defaultOutputPath: Driver["defaultOutputPath"] = `${appDirectory}/routes.d.ts`;
@@ -8,31 +8,42 @@ export const watchPaths: Driver["watchPaths"] = async () => [
 ];
 
 type SolidStartRouteData = {
-  id: string,
-  path: string,
-  componentPath: string,
-  dataPath: string
+  id: string;
+  path: string;
+  componentPath: string;
+  dataPath: string;
 };
 export const routes: Driver["routes"] = async () => {
-  const parsedRoutes = await new Promise<Array<SolidStartRouteData>>(async (resolve) => {
-    exec(`npx solid-start routes`, (error, stdout) => {
-      if (error != null) {
-        console.error("error", error);
-        return;
-      }
-      const routes = JSON.parse(stdout) as Array<SolidStartRouteData>;
-      resolve(routes);
-    });
-  });
+  const parsedRoutes = await new Promise<Array<SolidStartRouteData>>(
+    async (resolve) => {
+      exec(`npx solid-start routes`, (error, stdout) => {
+        try {
+          if (error != null) {
+            console.error("error", error);
+            resolve([]);
+          } else {
+            const routes = JSON.parse(stdout) as Array<SolidStartRouteData>;
+            const filteredRoutes = routes.filter((route) => {
+              return !route.path.includes("*");
+            });
+            resolve(filteredRoutes);
+          }
+        } catch (error) {
+          resolve([]);
+        }
+      });
+    }
+  );
   if (parsedRoutes.length === 0) {
-    throw new Error('Couldn\'t parse routes. This may be due to breaking changes in "solid-start".');
+    throw new Error(
+      'Couldn\'t parse routes. This may be due to breaking changes in "solid-start".'
+    );
   }
   return parsedRoutes;
 };
 
-
 module.exports = {
   defaultOutputPath,
   watchPaths,
-  routes
-}
+  routes,
+};
