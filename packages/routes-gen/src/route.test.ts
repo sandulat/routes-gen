c22179da63e2f2af6ca2b6b3cd3442c1cb347884
Caleb Williams <br0p0p@users.noreply.github.com>
2023-06-13 11:26:13 -0500

    Better naming for this test

--- packages/routes-gen/src/route.test.ts
+++ packages/routes-gen/src/route.test.ts
@@ -1,6 +1,6 @@
 import { route } from "./route";
 
-it("only replaces the first occurrence", () => {
+it("only replaces the first occurrence when param names share the same prefix", () => {
   expect(
     route("/podcasts/:topic/:topicId", { topic: "sports", topicId: "123" })
   ).toEqual("/podcasts/sports/123");
