1bf6fd6689df9fae877d6159bfcd7b8f6fc6ab18
Caleb Williams <br0p0p@users.noreply.github.com>
2023-06-13 12:09:24 -0500

    Refactor the routes function

--- packages/routes-gen/src/route.ts
+++ packages/routes-gen/src/route.ts
@@ -3,13 +3,25 @@ export function route<T extends string>(
   params?: Record<string, any>
 ): T {
   if (params) {
-    return Object.entries(params)
-      .reduce(
-        (result, [key, value]) =>
-          result.replace(new RegExp(`:${key}\\??`), value),
-        path as string
-      )
-      .replace(new RegExp("\\/:.+\\?", "g"), "") as T;
+    const segments = path.split(/\/+/).map((segment) => {
+      if (segment.startsWith(":")) {
+        const key = segment.replace(":", "").replace("?", "");
+
+        if (key in params) {
+          return params[key];
+        }
+
+        // If the segment is optional and it doesn't exist in params, return null to omit it from the resulting path
+        if (segment.endsWith("?")) {
+          return null;
+        }
+      }
+
+      return segment;
+    });
+
+    // Filter out any null/undefined segments and join remaining segments
+    return segments.filter((value) => value != null).join("/") as T;
   }
 
   return path;
